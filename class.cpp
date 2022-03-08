c6f7eeb6cc07e4756a9c812a2ddcdeee64003db1
kjm9547 <50660458+kjm9547@users.noreply.github.com>
2022-03-08 18:32:40 +0900

    Add files via upload
    fd

---
+++ class.cpp
@@ -0,0 +1,24 @@
+/*#include <iostream>
+using namespace std;
+
+class Circle{
+public:
+	int radius;
+	double getAreas();
+};
+
+double Circle::getAreas(){
+	return 3.14*radius*radius;
+}
+
+int main(){
+	Circle donut;
+	donut.radius = 1;
+	double area = donut.getAreas();
+	cout << "dounut 면적은" << area << endl;
+
+	Circle pizza;
+	pizza.radius = 20;
+	area = pizza.getAreas();
+	cout << "pizza 면적은" << area << endl;
+}*/
