41d34c070f09a067a6f3e174cb2c6c954ed67037
kjm9547 <50660458+kjm9547@users.noreply.github.com>
2022-03-10 23:06:05 +0900

    Add files via upload

--- class.cpp
+++ class.cpp
@@ -1,24 +1,40 @@
-/*#include <iostream>
+#include <iostream>
 using namespace std;
 
-class Circle{
+class Rectangle {
 public:
-	int radius;
-	double getAreas();
-};
+	int width;
+	int height;
+	bool isSquare();
+	Rectangle();
+	Rectangle(int a, int b);
+	Rectangle(int c);
 
-double Circle::getAreas(){
-	return 3.14*radius*radius;
+};
+bool Rectangle::isSquare() {
+	if (width == height) return true;
+	else return false;
+}
+Rectangle::Rectangle() {
+	width = 2;
+	height = 2;
+}
+Rectangle::Rectangle(int a, int b) {
+	width = a;
+	height = b;
+}
+Rectangle::Rectangle(int c) {
+	width = c;
+	height = c;
 }
 
-int main(){
-	Circle donut;
-	donut.radius = 1;
-	double area = donut.getAreas();
-	cout << "dounut 면적은" << area << endl;
+int main() {
+	Rectangle rect1;
+	Rectangle rect2(3,5);
+	Rectangle rect3(3);
 
-	Circle pizza;
-	pizza.radius = 20;
-	area = pizza.getAreas();
-	cout << "pizza 면적은" << area << endl;
-}*/
+	if (rect1.isSquare()) cout << "yes" << endl;
+	if (rect2.isSquare()) cout << "yes" << endl;
+	if (rect3.isSquare()) cout << "yes" << endl;
+
+}
