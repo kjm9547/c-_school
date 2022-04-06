8cce92a6351b2056cd2bfdb17b873b04f835ea01
kjm9547 <50660458+kjm9547@users.noreply.github.com>
2022-04-06 12:36:03 +0900

    Update 4_5.cpp
    update_code
    최적화

--- CP_4/4_5.cpp
+++ CP_4/4_5.cpp
@@ -1,34 +1,25 @@
-#include <iostream>
-#include <string>
-#include <cstdlib>
-#include <ctime>
+#include<iostream>
+#include<string>
+#include<cstdlib>
+#include<ctime>
 using namespace std;
 int main() {
-	string s;
-	string change = "";
-	string space = "";
-	//rand() % (b - a + 1) + a //a~b±îÁö
-	getline(cin, s, '\n');
-	while (true)
-	{
-		srand((unsigned)time(0));
-		int len = s.length();
-		int n = rand() % len;
-		space = s.substr(n,1);
-		
-		if (space != " ") {
-
-		srand((unsigned)time(0));
-		char b = 'a' + (rand() % 26);
-		
-		change += b;
-		s.replace(n, 1, change);
-		break;
+	string text;
+	
+	int ran_alpha;
+	getline(cin, text);
+	int len = text.length();
+	srand((unsigned)time(0));
+	while (true) {
+		int ran = rand() % (len - 1 - 0 + 1) + 0;
+		if (text[ran] != ' ') {
+			ran_alpha = rand() % (26 - 0 + 1) + 0;
+			text[ran] = ran_alpha + 'a';
+			
+			break;
 		}
 	}
-	cout << s <<endl;
 
-	cout << space;
-
-
+	cout << text;
+	
 }
