2ca67f37716d80d77786bbc825dddc429c676ddb
kjm9547 <50660458+kjm9547@users.noreply.github.com>
2022-05-06 21:54:14 +0900

    Update 6_2.cpp

--- CP_6/6_2.cpp
+++ CP_6/6_2.cpp
@@ -28,3 +28,26 @@ int main() {
 	ashley.show();
 	helen.show();
 }
+///////////////////////(2)
+#include <iostream>
+using namespace std;
+class Person {
+	int id;
+	double weight;
+	string name;
+public:
+
+	Person(int id =1 , string name = "grace", double weight = 20.5) {
+		this->id = id;
+		this->name = name;
+		this->weight = weight;
+	}
+
+	void show() { cout << id << ' ' << weight << ' ' << name << endl; }
+};
+int main() {
+	Person grace, ashley(2, "Ashley"), helen(3, "Helen", 32.5);
+	grace.show();
+	ashley.show();
+	helen.show();
+}
