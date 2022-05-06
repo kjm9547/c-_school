0ebc4d09e2c87d401d6166a4d3a74140d754d0e2
kjm9547 <50660458+kjm9547@users.noreply.github.com>
2022-05-06 21:45:43 +0900

    Update cp6_2.cpp

--- CP_6/cp6_2.cpp
+++ CP_6/cp6_2.cpp
@@ -26,3 +26,29 @@ int main() {
 	int z = big(30, 60, 50);
 	cout << x << ' ' << y << ' ' << z << endl;
 }
+//////////////////////(2)
+#include <iostream>
+using namespace std;
+int big(int a, int b, int max =100) {
+	if (a > b) {
+		if (max > a) {
+			return a;
+		}
+		else
+			return max;
+	}
+	else {
+		if (max > b)
+			return b;
+		else
+			return max;
+	}
+}
+
+int main() {
+	int x = big(3, 5);
+	int y = big(300, 60);
+	int z = big(30, 60, 50);
+	cout << x << ' ' << y << ' ' << z << endl;
+
+}
