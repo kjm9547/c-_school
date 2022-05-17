bbfe87cfba2171de74c8149ead911ee3e3c010cf
kjm9547 <50660458+kjm9547@users.noreply.github.com>
2022-05-17 22:43:33 +0900

    Add files via upload

--- cp_8/cp_8_9.cpp
+++ cp_8/cp_8_9.cpp
@@ -1,115 +1,145 @@
 #include<iostream>
 using namespace std;
-//예약 시스템 작동
-class AirlineBook:public Console {
-	Schedule* s;
-	
+class Seat {
+	string  name;
+
 public:
-	AirlineBook() {
-		s = new Schedule[3];
-		s[0].setTime("07시");
-		s[1].setTime("12시");
-		s[2].setTime("17시");
-	}
-	void run() {
-		cout << "************ 한성항공에 오신것을 환영합니다 ************" << endl;
-		while (true)
-		{
-			menu();
-			switch (select_menu)
-			{
-			case 1: {
-				reservation();
-			}
-			case 2: {
-
-			}
-			case 3: {
-
-			}
-			case 4: {
-
-			}
-
-			default:
-				break;
-			}
-		}
-	}
+	string getName() { return name; }
+	void setName(string name) { this->name = name; }
 };
-//메뉴출력 함수 메뉴선택 비행 시간 사용자 이름 좌석 번호등을 입력받는 함수 
-class Console{
-protected:
-	static int select_menu;
-	static int choice_time;
-	static int s_num;
-	static string name;
-	
-public:
-	static void menu() {
-		cout << "예약:1 취소:2 보기:3 끝내기:4>>" << endl;
-		cin >> select_menu;	
-	}
-	static void reservation() {
-		cout << "07시:1 12시:2 17시:3>>" << endl;
-		cin >> choice_time;
-		
-		selectSeat();
-		selectUser();
-		
-	}
-	static void selectSeat() {
-		cout << "좌석 번호>>" << endl;
-		cin >> s_num;
-	}
-	static void selectUser() {
-		cout << "이름 입력>>"<<endl;
-		cin >> name;
-	}
-};
-//하나의 스케줄 구현 이름 저장 좌석에 대한 예약 취소 보기등 관리
-class Schedule{
+class Schedule {
 	Seat* seat;
 	string time;
 public:
 	Schedule() {
 		seat = new Seat[8];
-	}
-	//에약
-	void check_in() {
-
-	}
-	//취소
-	void cancel_reserve() {
-
-	
-	}
-	void setTime(string time) {
-		this->time = time;
-	}
-	void show() {
 		for (int i = 0; i < 8; i++) {
-			cout << seat[i].getSeat() << '   ';
+			seat[i].setName("---");
+		}
+	}
+	void show_seat() {
+		for (int i = 0; i < 8; i++) {
+			cout << seat[i].getName() << '\t';
 		}
 		cout << endl;
 	}
+	string getTime() { return time; }
+	void setTime(string time) { this->time = time; }
+	void reserve(int a, string b) {
+		seat[a].setName(b);
+	}
+	void cancel_seat(int a, string b) {
+		if (seat[a].getName() == b) {
+			seat[a].setName("---");
+		}
+	}
 };
-//좌석을 구현 이름 저장 좌석에 대한 예약 및 취소 보기
-class Seat{
-	string available_seat;
-	string name;
-public:
 
-	Seat() {
-		available_seat = "---";
-		name = "";
+class Console {
+	int select_menu;
+	int input_num;
+	int select_time;
+	string reserv_name;
+public:
+	int menu() {
+		cout << "예약:1 취소:2 보기3: 끝내기:4>>";
+		cin >> select_menu;
+		return select_menu;
 	}
+	int reserve_num() {
+		cout << "좌석번호>>";
+		cin >> input_num;
+		return input_num;
+	}
+	string reserve_name() {
+		cout << "이름입력>>";
+		cin >> reserv_name;
+		return reserv_name;
+	}
+
+	int time() { cin >> select_time; return select_time; }
+};
+class Airlinebook {
+	Schedule* s;
+	Console c;
+	int menu;
+	int time;
 	
-	string getSeat() {
-		return available_seat;
+public:
+	Airlinebook() {
+		menu = 0;
+		time = 0;
+		s = new Schedule[3];
+		s[0].setTime("0시");
+		s[1].setTime("7시");
+		s[2].setTime("12시");
+	}
+	void run() {
+		int r_num;
+		string r_name;
+		cout << "*********** 항공예약 ***********" << endl;
+		while (true) {
+			menu = c.menu();
+			if (menu == 1) {
+				cout << s[0].getTime() << ":1 " << s[1].getTime() << ":2 " << s[2].getTime() << ":3 ";
+				time = c.time();
+				if (time == 1) {
+					s[0].show_seat();
+					r_num = c.reserve_num();
+					r_name = c.reserve_name();
+					s[0].reserve(r_num-1,r_name);
+				}
+				else if (time == 2) {
+					s[1].show_seat();
+					r_num = c.reserve_num();
+					r_name = c.reserve_name();
+					s[1].reserve(r_num - 1, r_name);
+				}
+				else if (time == 3) {
+					s[2].show_seat();
+					r_num = c.reserve_num();
+					r_name = c.reserve_name();
+					s[2].reserve(r_num - 1, r_name);
+				}
+
+			}
+			else if (menu == 2) {
+				cout << s[0].getTime() << ":1 " << s[1].getTime() << ":2 " << s[2].getTime() << ":3 ";
+				time = c.time();
+				if (time == 1) {
+					s[0].show_seat();
+					r_num = c.reserve_num();
+					r_name = c.reserve_name();
+					s[0].cancel_seat(r_num - 1, r_name);
+				}
+				else if (time == 2) {
+					s[1].show_seat();
+					r_num = c.reserve_num();
+					r_name = c.reserve_name();
+					s[1].cancel_seat(r_num - 1, r_name);
+				}
+				else if (time == 3) {
+					s[2].show_seat();
+					r_num = c.reserve_num();
+					r_name = c.reserve_name();
+					s[2].cancel_seat(r_num - 1, r_name);
+				}
+
+			}
+			else if (menu == 3) {
+					s[0].show_seat();
+					s[1].show_seat();
+					s[2].show_seat();
+			}
+			else if (menu == 4) {
+				cout << "시스템을 종료합니다.";
+				break;
+			}
+		}
 	}
 };
+
 int main() {
-	AirlineBook a;
+	Airlinebook a;
 	a.run();
 }
